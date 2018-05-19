f551b2a291df2871e5e2ee6dde793d170187729b
duzhijian <1024192224@qq.com>
2018-05-19 09:32:47 -0700

    99chengfabiao

---
+++ a.java
@@ -0,0 +1,16 @@
+public class a
+{
+	public static void main(String[] args)
+	{	
+		int a=1;
+		for(;a<=9;a++)
+		{
+			for(int b=1;b<=a;b++)
+			{
+				System.out.print(b+"*"+a+"="+(a*b)+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}		
